cb4f91a05242117a7455dfe4edd97721dc765fc5
Joel Koepke <Joel.Koepke@gmail.com>
2023-12-17 17:01:18 -0600

    test with packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="grafana_screenshot_helper",
-    version="0.2",
-    py_modules=["grafana_screenshot_helper"],
-    # packages=find_packages(),
+    version="0.3",
+    # py_modules=["grafana_screenshot_helper"],
+    packages=["grafana_screenshot_helper"],
 )
