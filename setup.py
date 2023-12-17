a3747224d3fef1838ad916c2e7e7b208aeb34ed5
Joel Koepke <Joel.Koepke@gmail.com>
2023-12-17 17:08:50 -0600

    add requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+install_requires = ["requests"]
+
 setup(
     name="grafana_screenshot_helper",
     version="0.3",
     # py_modules=["grafana_screenshot_helper"],
     packages=["grafana_screenshot_helper"],
+    include_package_data=True,
+    install_requires=install_requires,
+    zip_safe=False,
 )
