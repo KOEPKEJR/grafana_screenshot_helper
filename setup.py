59641c9cee4ddf1d6b0d6ceba70342bd149fda5e
Joel Koepke <Joel.Koepke@gmail.com>
2023-12-17 10:31:36 -0600

    File explicity

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="grafana_screenshot_helper",
     version="0.1",
-    # py_modules=["grafana_screenshot_helper"],
-    packages=find_packages(),
+    py_modules=["grafana_screenshot_helper"],
+    # packages=find_packages(),
 )
